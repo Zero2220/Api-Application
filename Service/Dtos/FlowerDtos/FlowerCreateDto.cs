7bbd3625050eb56e97118589179a3d49d417ca6f
anarpi <anarpi@code.edu.az>
2024-06-27 21:45:07 +0400

    new

---
+++ Service/Dtos/FlowerDtos/FlowerCreateDto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Dtos.FlowerDtos
+{
+    public class FlowerCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price {  get; set; }
+        public int CategoryId { get; set; }
+        public List<IFormFile> FormFiles { get; set; }
+        
+
+    }
+}
---
+++ Service/Dtos/FlowerDtos/FlowerEditDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Dtos.FlowerDtos
+{
+    public class FlowerEditDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int CategoryId { get; set; }
+        public List<IFormFile> FormFiles { get; set; }
+        public List<int>? FileIds { get; set; }
+    }
+}
